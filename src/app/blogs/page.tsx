b9c7ca536ac0d86795cb83c6608ab77ae85be210
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-03 23:43:22 +0600

    blogs page to show all post

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const BlogsPage = () => {
-  return <div>BlogsPage</div>;
+  return (
+    <div>
+      {" "}
+      <h1 className="text-center text-4xl my-5">
+        All Blog Post from <span className="text-accent">Blog stack</span>
+      </h1>
+      <p className=" text-xl text-center text-gray-400 w-2/4 mx-auto">
+        <i>
+          {" "}
+          Dive into the fascinating world of quantum computing, where unlocking
+          unprecedented computational power.
+        </i>
+      </p>
+    </div>
+  );
 };
 
 export default BlogsPage;
