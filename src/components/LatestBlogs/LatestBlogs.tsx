8e677ba51da96fb6918b0df76c77ee79bf74fbca
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-03 23:00:16 +0600

    latest blog

--- src/components/LatestBlogs/LatestBlogs.tsx
+++ src/components/LatestBlogs/LatestBlogs.tsx
@@ -1,7 +1,9 @@
 const LatestBlogs = () => {
   return (
     <div>
-      <h1>Latest Post from Blog stack</h1>
+      <h1 className="text-center text-4xl my-5">
+        Latest Post from <span className="text-accent">Blog stack</span>
+      </h1>
     </div>
   );
 };
