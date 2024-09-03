b133b03da14a61b5af27f30768c911d287d3649b
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-04 01:00:51 +0600

    data fetching

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,3 +1,6 @@
+import BlogDetails from "@/components/ui/BlogDetails";
+import { IBlog } from "@/types";
+
 interface BlogId {
   params: {
     blogId: string;
@@ -6,9 +9,13 @@ interface BlogId {
 const BlogDetailsPage = async ({ params }: BlogId) => {
   const { blogId } = params;
   const response = await fetch(`http://localhost:5000/blogs/${blogId}`);
-  const blog = await response.json();
+  const blog: IBlog = await response.json();
   console.log(blog);
-  return <div>BlogDetailsPage</div>;
+  return (
+    <div>
+      <BlogDetails blog={blog} />
+    </div>
+  );
 };
 
 export default BlogDetailsPage;
