27fed55fa9657cc31200d1f0cfcd5f50b8767083
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-03 23:04:41 +0600

    latest blog data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import LatestBlogs from "@/components/LatestBlogs/LatestBlogs";
 import LatestBlogCard from "@/components/ui/LatestBlogCard";
 
 const HomePage = async () => {
+  const response = await fetch("http://localhost:5000/blogs");
+  const blogs = await response.json();
+  console.log(blogs);
   return (
     <>
       <h1 className="text-center text-4xl my-5">
-        {" "}
         <LatestBlogs />
       </h1>
     </>
