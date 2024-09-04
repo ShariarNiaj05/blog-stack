68881b5c27f2015985fcf07d24b477c0636c3028
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-04 22:10:32 +0600

    generate Static Params

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -6,6 +6,14 @@ interface BlogId {
     blogId: string;
   };
 }
+
+export const generateStaticParams = async () => {
+  const response = await fetch(`http://localhost:5000/blogs`);
+  const blogs = await response.json();
+
+  return blogs.slice(0, 3).map((blog: IBlog) => ({}));
+};
+
 const BlogDetailsPage = async ({ params }: BlogId) => {
   const { blogId } = params;
   const response = await fetch(`http://localhost:5000/blogs/${blogId}`, {
