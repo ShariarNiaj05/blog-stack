320c50f3075e19cadf348d766270e36a39512baa
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-05 00:36:37 +0600

    Loading UI

--- src/app/blogs/loading.tsx
+++ src/app/blogs/loading.tsx
@@ -1,7 +1,18 @@
+import LoadingCard from "@/components/ui/LoadingCard";
+import { IBlog } from "@/types";
+
 const BlogLoadingPage = async () => {
   const response = await fetch("http://localhost:5000/blogs");
   const blogs = await response.json();
-  return <div></div>;
+  return (
+    <div>
+      <div className="grid grid-cols-4 gap-4">
+        {blogs?.map((blog: IBlog) => (
+          <LoadingCard key={blog.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BlogLoadingPage;
