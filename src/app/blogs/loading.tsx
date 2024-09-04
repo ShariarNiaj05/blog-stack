50fe9e190080d5ff0bf29a8ad37f5c5cec87d75e
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-05 00:32:28 +0600

    Loading UI

---
+++ src/app/blogs/loading.tsx
@@ -0,0 +1,9 @@
+const BlogLoadingPage = () => {
+  return (
+    <div>
+      <h1 className=" text-4xl text-center text-red-500">BlogLoadingPage</h1>
+    </div>
+  );
+};
+
+export default BlogLoadingPage;
